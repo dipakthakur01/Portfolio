849eb753a9ab0793544b5dd41fdc2dabf94d2424
Dipak Thakur <rustamchupa473@gmail.com>
2025-09-09 19:37:48 +0545

    Changes

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -13,7 +13,7 @@ import {
 import { home } from "./index";
 
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL: string = "https://demo.magic-portfolio.com";
+const baseURL: string = "https://github.com/dipakthakur01/Portfolio";
 
 const routes: RoutesConfig = {
   "/": true,
